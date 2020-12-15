adabd01dd6afb30afc63bddfb2969da2823e28a1
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-12-14 21:14:01 -0300

    Adding front deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const base = 'http://localhost:3000/api/v1'
+const url_api = process.env.api_url
+// const base = 'http://api-joaolucas.herokuapp.com/api/v1'
 
 const API = axios.create({
-  baseURL: base
+  baseURL: url_api
 })
 
 export default API
